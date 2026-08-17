7a8e6b87e4bae64a78eda3c525dc95c96cdc3aff
gogshelidzegiorgi13-stack <gogshelidzegiorgi13@gmail.com>
2026-08-17 16:07:37 +0400

    Update main.jsx with real Google Client ID

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// 📌 შენი Google Client ID ჩასმულია აქ:
+const GOOGLE_CLIENT_ID = "18572748717-ld11gv498d85cfg671vn25jv4tsgossl.apps.googleusercontent.com"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 )
