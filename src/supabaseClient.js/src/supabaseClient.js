0499ff16170af588730574be147c99b978660b00
gogshelidzegiorgi13-stack <gogshelidzegiorgi13@gmail.com>
2026-08-17 19:37:56 +0400

    Create supabaseClient file

--- src/supabaseClient.js/src/supabaseClient.js
+++ src/supabaseClient.js/src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'sb_publishable_nbbT_om7rJ_8ZUHuH9cpjA_QRndiTaD'
-
-const supabaseAnonKey = 'sb_publishable_nbbT_om7rJ_8ZUHuH9cpjA_QRndi...' // 👈 შენი დაკოპირებული Publishable key
+const supabaseUrl = 'https://jngzlorzhnzcffgoxdzn.supabase.co'
+const supabaseAnonKey = 'sb_publishable_nbbT_om7rJ_8ZUHuH9cpjA_QRndi...' // 👈 აქ შენი სრული Publishable Key ჩასვი
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
